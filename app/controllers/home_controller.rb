9d9dae02a243b52ba8a90b8777e20d92bb94ebcb
Calla Patel <callapatel@gmail.com>
2014-10-06 13:52:09 -0700

    first of the rails blog prac

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+    # puts "#*********"
+    #simmilar to sinatras myapp page for get methods and post methods.
+  end
+end
+#looks in the views to find its matching directory: home. then looks for a file with the same name as the method.
+#we don't tell it where to look. this is where it goes to read the files.
+#this is where all the blog post logic will be shown - if I want all my blog posts to render.
